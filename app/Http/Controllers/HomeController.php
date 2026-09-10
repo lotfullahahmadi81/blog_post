35c2c704fcaefb78e7bccdbfcc3039a40d4b2883
lotfullahahmadi81 <lotfullahahmadi81@gmail.com>
2026-09-10 21:08:07 +0430

    show the post in the frontend done

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('frontend.home.index')->with('posts', Post::orderBy('created_at', 'desc')->paginate(8));
+    }
+
+    public function show($slug){
+        $post = Post::where('slug', $slug)->first();
+        return view('frontend.home.show')->with('post', $post);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\backend\PostController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('frontend.home.index');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/post/{slug}', [HomeController::class, 'show'])->name('post.show');
 
 Route::get('/about', function () {
     return view('frontend.about.index');
